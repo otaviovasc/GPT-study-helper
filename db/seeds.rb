642624a6fd8e5ab284e7a6eab48602f3da35f887
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-10-04 02:28:48 -0300

    Scrapper done and DB populated. I also adjusted somethings in the react view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,102 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'httparty'
+require 'nokogiri'
 
-Subject.destroy_all
-SubSubject.destroy_all
 Exercise.destroy_all
+SubSubject.destroy_all
+Subject.destroy_all
 
-s1 = Subject.create(name: "Matematica")
-s2 = Subject.create(name: "Historia")
-ss1 = SubSubject.create(name: "Aritmetica", subject: s1)
-ss2 = SubSubject.create(name: "1500 brasil", subject: s2)
-Exercise.create(question: "01 aritmetica louca", answer: "a", sub_subject: ss1)
-Exercise.create(question: "02 aritmetica crazy", answer: "b", sub_subject: ss1)
-Exercise.create(question: "01 historia maneira", answer: "a", sub_subject: ss2)
-Exercise.create(question: "02 historia bacana", answer: "a", sub_subject: ss2)
+URLS = [
+  ["https://projetoagathaedu.com.br/banco-de-questoes/biologia.php", "Biologia"],
+  ["https://projetoagathaedu.com.br/banco-de-questoes/matematica.php", "Matemática"],
+  ["https://projetoagathaedu.com.br/banco-de-questoes/geografia-sociologia-e-filosofia.php", "Geografia, Filosofia e Sociologia"],
+  ["https://projetoagathaedu.com.br/banco-de-questoes/linguagens.php", "Linguagens"],
+  ["https://projetoagathaedu.com.br/banco-de-questoes/historia.php", "História"],
+  ["https://projetoagathaedu.com.br/banco-de-questoes/fisica.php", "Física"],
+  ["https://projetoagathaedu.com.br/banco-de-questoes/quimica.php", "Química"]
+]
+
+def extract_exercises_from_page(sub_subject, url)
+    response = HTTParty.get(url)
+    page = Nokogiri::HTML(response.body)
+
+    # Check if page exists
+    if page.text.include?("File not found.")
+      puts "Skipping #{url} due to 'File not found.'"
+      return
+    end
+    puts "Processing #{url}"
+
+    # Handle edge case where the #tabela-respostas doesnt have 2 tables
+    unless page.css("#gabarito #tabela-respostas")[1].nil?
+      answers = page.css("#gabarito #tabela-respostas")[1].css("td").map { |td| td.text[/[A-Z]/] }
+    else
+      answers = page.css("#gabarito #tabela-respostas")[0].css("td").map { |td| td.text[/[A-Z]/] }
+    end
+
+    index = 0 # This index will be used to access the correct answer from the answers array for each exercise
+
+    page.css(".questoes-enem-vestibular").each do |exercise_container|
+      img_tags = [] # Array to store all the image URLs within the current exercise
+
+      question_paragraphs = exercise_container.css("p:not(.questoes-enem-vestibular-foco):not(.direita)")
+      question_text_parts = question_paragraphs.map do |paragraph|
+        img = paragraph.css("img")
+        if img.any?
+          img_tags << img.first['data-src']
+          img.remove
+        end
+        paragraph.inner_html.strip
+      end
+
+      question_text = question_text_parts.join("\n")
+
+      options = exercise_container.css("ol li").map do |option_node|
+        img = option_node.css("img")
+        if img.any?
+          img_url = img.first["data-src"]
+          img_url
+        else
+          option_node.text.strip
+        end
+      end
+
+      direita_paragraphs = exercise_container.css("p.direita").map(&:text)
+
+      Exercise.create!(
+        question: question_text,
+        img_tag: img_tags,
+        options: options,
+        direita: direita_paragraphs,
+        sub_subject: sub_subject,
+        answer: answers[index]
+      )
+
+      index += 1
+    end
+end
+
+URLS.each do |url, subject_name|
+  # Create the subject
+  subject = Subject.find_or_create_by!(name: subject_name)
+
+  # For each URL, scrape the page to get a list of sub-subjects
+  response = HTTParty.get(url)
+  page = Nokogiri::HTML(response.body)
+  page.css(".accordion .opcao a").each do |link|
+    sub_subject_name = link.css(".lista-tema").text.strip
+    relative_path = link['href']
+    escaped_path = relative_path.split('/').map { |part| CGI.escape(part) }.join('/')
+    sub_subject_url = URI.join(url, escaped_path).to_s
+
+    # Skip the URL if it's one of the main subject URLs
+    next if URLS.any? { |subject_url, _| subject_url == sub_subject_url }
+
+    # Create or find the sub_subject and link it to the subject
+    sub_subject = SubSubject.find_or_create_by!(name: sub_subject_name, subject: subject)
+
+    # Extract exercises for this sub-subject
+    extract_exercises_from_page(sub_subject, sub_subject_url)
+  end
+end
+
+puts "Seeding done!"
--- frontend/features/exercises/ExercisesList.jsx
+++ frontend/features/exercises/ExercisesList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { API_URL } from "../../constants";
+import './ExercisesList.css';
 
 const ExercisesList = ({ selectedSubSubject }) => {
   const [exercises, setExercises] = useState([]);
@@ -33,15 +34,69 @@ const ExercisesList = ({ selectedSubSubject }) => {
     }));
   };
 
+  function processQuestionText(text) {
+    const BREAK_LIMIT = 40;
+    let charCounter = 0;
+    const elements = [];
+    let currentSentence = "";
+
+    text.split("").forEach((char, idx) => {
+      currentSentence += char;
+      charCounter++;
+
+      // If we hit a breakpoint condition
+      if (
+        (char === "." || char === "?") &&
+        charCounter > BREAK_LIMIT
+      ) {
+        elements.push(<span key={idx}>{currentSentence}<br/><br/></span>);
+        currentSentence = "";
+        charCounter = 0;
+      }
+    });
+
+    // Add remaining part
+    if (currentSentence) {
+      elements.push(<span key="last">{currentSentence}</span>);
+    }
+
+    return elements;
+  }
+
   return (
     <div>
       {exercises.map((exercise) => (
-        <div key={exercise.id}>
-          <h2>{exercise.question}</h2>
+        <div key={exercise.id} className="exercise-container">
+          <h2>{processQuestionText(exercise.question)}</h2>
+
+          {/* Render images from img_tag */}
+          {exercise.img_tag && exercise.img_tag.map((imgSrc, idx) => (
+            <img key={idx} src={imgSrc} alt="Exercise illustration" />
+          ))}
+
+          {/* Render direita text */}
+          {exercise.direita && exercise.direita.map((text, idx) => (
+            <p key={idx}>{text}</p>
+          ))}
+
+          {/* Render options */}
+          <ul>
+            {exercise.options && exercise.options.map((option, idx) => {
+              const isImage = option.startsWith("http");
+              const optionLetter = String.fromCharCode(65 + idx); // 65 is ASCII for 'A'
+              return (
+                <h3 key={idx}>
+                  {optionLetter}. {isImage ? <img src={option} alt={`Option ${optionLetter}`} /> : option}
+                </h3>
+              );
+            })}
+          </ul>
+
           <button onClick={() => toggleAnswerVisibility(exercise.id)}>
             {answerVisibility[exercise.id] ? "Esconder Resposta" : "Mostrar Resposta"}
           </button>
-          {answerVisibility[exercise.id] && <p>{exercise.answer}</p>}
+
+          {answerVisibility[exercise.id] && <h4>{exercise.answer}</h4>}
         </div>
       ))}
     </div>
